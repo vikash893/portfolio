8ba0ae4341d3fb5d6e2b99dcd8c3ddb2657a5129
vikash893 <vikashbhardwaj430@gmail.com>
2025-08-27 23:48:42 +0530

    Your descriptive commit message here

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -86,6 +86,19 @@ const Interests = () => {
           </div>
         </div>
       </div>
+      <div className="mt-16 text-center">
+          <div className="bg-white rounded-2xl p-8 shadow-lg max-w-4xl mx-auto">
+            <h3 className="text-2xl font-bold text-gray-900 mb-4">
+              Feedback
+            </h3>
+            <img
+                src="/feedback.png"
+                alt="Feedback"
+                className="mx-auto w-100 h-[550px] lg:w-100 lg:h-[550px] rounded-2xl object-cover shadow-2xl"
+              />
+          </div>
+        
+      </div>
     </section>
   );
 };
