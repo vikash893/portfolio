088e211897a0f6ed1a2c39f6c985cc10019f9074
vikash893 <vikashbhardwaj430@gmail.com>
2025-12-03 23:34:18 +0530

    update 2nd feedback size2

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -108,7 +108,7 @@ const Interests = () => {
             <img
                 src="/feedback2.png"
                 alt="Feedback"
-                className="mx-auto w-[600px] h-[400px] lg:w-[700px] lg:h-[500px] rounded-2xl object-cover shadow-2xl"
+                className="mx-auto w-[800px] h-[400px] lg:w-[800px] lg:h-[500px] rounded-2xl object-cover shadow-2xl"
               />
               
           </div>
